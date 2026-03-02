2169f5fe7605d41b047cfb5be70599bfa9f0aa14
kajal-1424 <kajalnazirkar781@gmail.com>
2026-03-02 04:43:12 -0800

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,14 @@
+#string iterate usng range
+s="python" 
+for i in range(len(s)):
+    print(i,"----->",s[i])
+
+#List iterate using range
+l=[10,20,30,40]
+for i in range(len(l)):
+    print(i,"------>",l[i])
+
+#Tuple iterate using range
+t=(100,200,300,400)
+for i in range(len(t)):
+    print(i,"----->",t[i])    
